b78eea2c97d2a6ef98131cc17e8320d4f56e2a7b
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-07-19 12:20:08 +0300

    update

--- messaging_app/chat/views.py
+++ messaging_app/chat/views.py
@@ -1,14 +1,11 @@
 from rest_framework import viewsets
 from .models import Message, Conversation
 from .serializers import MessageSerializer, ConversationSerializer
-from rest_framework.permissions import IsAuthenticated
-
-class ConversationViewSet(viewsets.ModelViewSet):
-    queryset = Conversation.objects.all()
-    serializer_class = ConversationSerializer
-    permission_classes = [IsAuthenticated]
 
 class MessageViewSet(viewsets.ModelViewSet):
     queryset = Message.objects.all()
     serializer_class = MessageSerializer
-    permission_classes = [IsAuthenticated]
+
+class ConversationViewSet(viewsets.ModelViewSet):
+    queryset = Conversation.objects.all()
+    serializer_class = ConversationSerializer
