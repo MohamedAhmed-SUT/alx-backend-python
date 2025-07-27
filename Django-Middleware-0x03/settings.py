6532d8bab49c7afb4ab3f263353b317992b93089
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-07-27 17:20:25 +0300

    update

--- Django-Middleware-0x03/settings.py
+++ Django-Middleware-0x03/settings.py
@@ -30,3 +30,21 @@ MIDDLEWARE = [
     'chats.middleware.RestrictAccessByTimeMiddleware',
     'chats.middleware.RequestLoggingMiddleware',
 ]
+# in Django-Middleware-0x03/settings.py
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware', # This middleware adds request.user
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    # Your custom middleware
+    # Role check should be one of the first custom checks.
+    'chats.middleware.RolePermissionMiddleware',
+    'chats.middleware.OffensiveLanguageMiddleware',
+    'chats.middleware.RestrictAccessByTimeMiddleware',
+    'chats.middleware.RequestLoggingMiddleware',
+]
