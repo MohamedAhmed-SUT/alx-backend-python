7ad049aee6cf990fb67146546e2506c1cd26b757
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-07-27 16:37:36 +0300

    Create settings.py

---
+++ Django-Middleware-0x03/messaging_app/settings.py
@@ -0,0 +1,14 @@
+# in Django-Middleware-0x03/messaging_app/settings.py
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
+    # Add your custom middleware here
+    'chats.middleware.RequestLoggingMiddleware', 
+]
