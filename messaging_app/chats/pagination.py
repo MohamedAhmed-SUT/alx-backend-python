5bddf599a122d97086230baca298629f5914d5c8
MohamedAhmed-SUT <mohamedahmed2023sut@gmail.com>
2025-07-27 04:36:45 +0300

    update

---
+++ messaging_app/chats/pagination.py
@@ -0,0 +1,12 @@
+# messaging_app/chats/pagination.py
+
+from rest_framework.pagination import PageNumberPagination
+
+class MessagePagination(PageNumberPagination):
+    """
+    Custom pagination class for messages.
+    Sets the default page size to 20.
+    """
+    page_size = 20
+    page_size_query_param = 'page_size'
+    max_page_size = 100
